99c70670d120e180f2ae74ff519970fb79bbf7a5
Burak <sarisahinburak22@gmail.com>
2021-10-22 00:35:06 +0200

    js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.listen(3300,()=>{
     console.log('Node server created at port 3300');
 }); */
 
-//server.js
+/* //server.js
 const http = require('http'),
 
 makeServer = function (request,response){
@@ -25,4 +25,36 @@ server = http.createServer(makeServer);
 
 server.listen(3300,()=>{
     console.log('Node server created at port 3300');
+}); */
+
+//server.js
+const http = require('http'),
+    url = require('url'),
+
+makeServer = function (request,response){
+let path = url.parse(request.url).pathname;
+console.log(path);
+if(path === '/'){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.write('Hello world');
+}
+else if(path === '/about'){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.write('About page');
+}
+else if(path === '/blog'){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.write('Blog page');
+}
+else{
+    response.writeHead(404,{'Content-Type':'text/plain'});
+    response.write('Error page');
+}
+response.end();
+},
+server = http.createServer(makeServer);
+server.listen(3300,()=>{
+console.log('Node server created at port 3300');
 });
+
+
